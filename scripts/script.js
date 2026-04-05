37a36226ad71e8f46de8c74a986aaafc489391ca
Noel Tucker <noelstucker@gmail.com>
2026-04-05 09:17:32 -0400

    Refactor HTML structure and update button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -4,241 +4,240 @@ title.innerHTML = document.querySelector('h1').innerHTML === 'Home' ? 'Federatio
 
 // Design
 document.querySelector('#element-eight').outerHTML = `
-  <div id="element-1"></div>
-  <div id="element-2"></div>
-  <div id="element-3"></div>
-  <div id="element-4"></div>
-  <div id="element-5"></div>
-  <div id="element-6"></div>
-  <div id="element-7"></div><!--
-  <article id="element-eight">
-    ${document.querySelector('#element-eight').innerHTML}
-    <footer id="footer">
-      <p>Text &copy; 2024&ndash;2025 Noel Tucker</p>
-      <p>All quotations on the Federation Database are used in a way believed to quality as fair use under United States copyright law. All other text is owned by Noel Tucker and is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0">Creative Commons Attribution-ShareAlike 4.0 International license</a>.</p>
-      <p>The design of the Federation Database is inspired by the LCARS design style created by Michael Okuda. The colors are from LCARS designs by Andrew Jarvis. All original aspects are owned by Noel Tucker and are not available under any license. All unoriginal aspects are owned by <a href="https://www.paramount.com">Paramount Skydance</a> and are used in a way believed to qualify as fair use under United States copyright law.</p>
-      <p>License and/or copyright information is listed individually for all images, video, and audio on the Federation Database.</p>
-      <p>The Federation Database is not and does not claim to be endorsed by, sponsored by, or affiliated with Paramount Skydance, the Star Trek franchise, or any affiliated entity.</p>
-      <p>All Star Trek trademarks and copyrights on the Federation Database are owned by Paramount Skydance or an affiliated entity and are used in a way believed to qualify as fair use under United States copyright law.</p>
-    </footer>
-  </article>-->
-  <div id="element-9"></div>
-  <div id="element-10"></div>
-  <div id="element-11"></div>
-  <div id="element-12"></div>
-  <div id="element-13"></div>
-  <div id="element-14"></div>
-  <div id="element-15"></div>
-  <div id="element-16"></div>
-  <a id="label" href="https://noeltucker.github.io/federation-database">Federation Database</a>
-  <div id="button-1" class="button gray"><span class="button-margin">LCARS 42028</span></div>
-  <div id="button-2" class="button orange"><span class="button-margin">6539-456</span></div>
-  <div id="button-3" class="button orange"><span class="button-margin">01-558695</span></div>
-  <div id="button-4" class="button orange"><span class="button-margin">02-771423</span></div>
-  <div id="button-5" class="button orange"><span class="button-margin">03-109549</span></div>
-  <div id="button-6" class="button gray"><span class="button-margin">04-896790</span></div>
-  <div id="button-7" class="button gray"><span class="button-margin">05-947165</span></div>
-  <div id="button-8" class="button orange"><span class="button-margin">06-906308</span></div>
-  <div id="button-9" class="button gray"><span class="button-margin">Recalibrate</span></div>
-  <div id="button-10" class="button gray"><span class="button-margin">Close</span></div>
-  <div id="button-11" class="button gray"><span class="button-margin">01-767386</span></div>
-  <div id="button-12" class="button gray"><span class="button-margin">02-406950</span></div>
-  <div id="button-13" class="button gray"><span class="button-margin">03-883192</span></div>
-  <div id="button-14" class="button gray"><span class="button-margin">04-547632</span></div>
-  <div id="button-15" class="button orange"><span class="button-margin">05-521358</span></div>
-  <div id="button-16" class="button orange"><span class="button-margin">06-684421</span></div>
-  <div id="button-17" class="button gray"><span class="button-margin">01-906788</span></div>
-  <div id="button-18" class="button gray"><span class="button-margin">02-786195</span></div>
-  <div id="button-19" class="button gray"><span class="button-margin">03-855803</span></div>
-  <div id="button-20" class="button gray"><span class="button-margin">04-261697</span></div><!--
-  <table id="cascade">
-    <tbody>
-      <tr class="row-one">
-        <td>2385</td>
-        <td></td>
-        <td></td>
-        <td>8578232</td>
-        <td></td>
-        <td></td>
-        <td>9</td>
-        <td>5789</td>
-        <td>3882</td>
-        <td>5893</td>
-        <td>3489</td>
-        <td></td>
-        <td></td>
-        <td>365</td>
-        <td>846</td>
-        <td>9798</td>
-      </tr>
-      <tr class="row-two">
-        <td>2064</td>
-        <td></td>
-        <td></td>
-        <td>20080720</td>
-        <td></td>
-        <td></td>
-        <td>4</td>
-        <td>9776</td>
-        <td>626</td>
-        <td></td>
-        <td>126</td>
-        <td></td>
-        <td></td>
-        <td>97</td>
-        <td>6165</td>
-        <td>6626</td>
-      </tr>
-      <tr class="row-three">
-        <td>834</td>
-        <td></td>
-        <td></td>
-        <td>729111</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>89</td>
-        <td>6589</td>
-        <td>6547</td>
-        <td>3465</td>
-        <td></td>
-        <td></td>
-        <td>867</td>
-        <td>2347</td>
-        <td>5762</td>
-      </tr>
-      <tr class="row-four">
-        <td>4768</td>
-        <td></td>
-        <td></td>
-        <td>8967284</td>
-        <td></td>
-        <td></td>
-        <td>7</td>
-        <td>9798</td>
-        <td>8969</td>
-        <td>476</td>
-        <td>8476</td>
-        <td></td>
-        <td></td>
-        <td>9479</td>
-        <td>982</td>
-        <td>8969</td>
-      </tr>
-      <tr class="row-five">
-        <td>685</td>
-        <td></td>
-        <td></td>
-        <td>3478</td>
-        <td></td>
-        <td></td>
-        <td>8</td>
-        <td>864</td>
-        <td>7146</td>
-        <td>347</td>
-        <td>429</td>
-        <td></td>
-        <td></td>
-        <td>840</td>
-        <td></td>
-        <td>897</td>
-      </tr>
-      <tr class="row-six">
-        <td>757</td>
-        <td></td>
-        <td></td>
-        <td>898990</td>
-        <td></td>
-        <td></td>
-        <td>1</td>
-        <td>200</td>
-        <td>285</td>
-        <td>923</td>
-        <td>387</td>
-        <td></td>
-        <td></td>
-        <td>238</td>
-        <td>578</td>
-        <td>5875</td>
-      </tr>
-      <tr class="row-seven">
-        <td>484</td>
-        <td></td>
-        <td></td>
-        <td>947589</td>
-        <td></td>
-        <td></td>
-        <td>6</td>
-        <td>569</td>
-        <td>68</td>
-        <td>678</td>
-        <td>56</td>
-        <td></td>
-        <td></td>
-        <td>584</td>
-        <td>678</td>
-        <td>476</td>
-      </tr>
-      <tr class="row-eight">
-        <td>8206</td>
-        <td></td>
-        <td></td>
-        <td>4735</td>
-        <td></td>
-        <td></td>
-        <td>5</td>
-        <td>201</td>
-        <td>647</td>
-        <td>1863</td>
-        <td>933</td>
-        <td></td>
-        <td></td>
-        <td>254</td>
-        <td>7913</td>
-        <td>4798</td>
-      </tr>
-      <tr class="row-nine">
-        <td>3472</td>
-        <td></td>
-        <td></td>
-        <td>1138</td>
-        <td></td>
-        <td></td>
-        <td>3</td>
-        <td>447</td>
-        <td></td>
-        <td>407</td>
-        <td>676</td>
-        <td></td>
-        <td></td>
-        <td>99</td>
-        <td>7103</td>
-        <td>3819</td>
-      </tr>
-      <tr class="row-ten">
-        <td>1701</td>
-        <td></td>
-        <td></td>
-        <td>33397</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>834</td>
-        <td>753</td>
-        <td>204</td>
-        <td>59</td>
-        <td></td>
-        <td></td>
-        <td>806</td>
-        <td>2042</td>
-        <td>5047</td>
-      </tr>
-    </tbody>
-  </table>-->
+	<div id="panel-1" class="panel"></div>
+	<div id="panel-2" class="panel">
+		<div id="container-1" class="button-container">
+			<div id="button-1" class="button orange">01-558695</div>
+			<div id="button-2" class="button orange">02-771423</div>
+			<div id="button-3" class="button orange">03-109549</div>
+			<div id="button-4" class="button gray">04-896790</div>
+			<div id="button-5" class="button gray">05-947165</div>
+			<div id="button-6" class="button orange">06-906308</div>
+		</div>
+	</div>
+	<article id="panel-3">
+		<div id="container-2" class="button-container">
+			<div id="container-3" class="button-container">
+				<div id="button-7" class="button gray">2561-395</div>
+				<div id="button-8" class="button gray">3405-465</div>
+			</div>
+			<div id="container-4" class="button-container">
+				<div id="button-9" class="button gray">01-767386</div>
+				<div id="button-10" class="button gray">02-406950</div>
+				<div id="button-11" class="button gray">03-883192</div>
+				<div id="button-12" class="button gray">04-547632</div>
+				<div id="button-13" class="button orange">05-521358</div>
+				<div id="button-14" class="button orange">06-684421</div>
+			</div>
+		</div>
+		<div id="container-5" class="button-container">
+			<div id="button-15" class="button gray">01-906788</div>
+			<div id="button-16" class="button gray">02-786195</div>
+			<div id="button-17" class="button gray">03-855803</div>
+			<div id="button-18" class="button gray">04-261697</div>
+		</div>
+		<a id="label" href="https://noeltucker.github.io/federation-database">Federation Database</a>
+		${document.querySelector('#element-eight').innerHTML}
+		<footer id="footer">
+			<p>Text &copy; 2024&ndash;2025 Noel Tucker</p>
+			<p>All quotations on the Federation Database are used in a way believed to quality as fair use under United States copyright law. All other text is owned by Noel Tucker and is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0">Creative Commons Attribution-ShareAlike 4.0 International license</a>.</p>
+			<p>The design of the Federation Database is inspired by the LCARS design style created by Michael Okuda. The colors are from LCARS designs by Andrew Jarvis. All original aspects are owned by Noel Tucker and are not available under any license. All unoriginal aspects are owned by <a href="https://www.paramount.com">Paramount Skydance</a> and are used in a way believed to qualify as fair use under United States copyright law.</p>
+			<p>License and/or copyright information is listed individually for all images, video, and audio on the Federation Database.</p>
+			<p>The Federation Database is not and does not claim to be endorsed by, sponsored by, or affiliated with Paramount Skydance, the Star Trek franchise, or any affiliated entity.</p>
+			<p>All Star Trek trademarks and copyrights on the Federation Database are owned by Paramount Skydance or an affiliated entity and are used in a way believed to qualify as fair use under United States copyright law.</p>
+		</footer>
+	</article>
+	<div id="button-panel-1" class="button gray">LCARS 42028</div>
+	<div id="button-panel-2" class="button orange">6539-456</div>
+	<table id="cascade">
+		<tbody>
+			<tr class="row-one">
+				<td>2385</td>
+				<td></td>
+				<td></td>
+				<td>8578232</td>
+				<td></td>
+				<td></td>
+				<td>9</td>
+				<td>5789</td>
+				<td>3882</td>
+				<td>5893</td>
+				<td>3489</td>
+				<td></td>
+				<td></td>
+				<td>365</td>
+				<td>846</td>
+				<td>9798</td>
+			</tr>
+			<tr class="row-two">
+				<td>2064</td>
+				<td></td>
+				<td></td>
+				<td>20080720</td>
+				<td></td>
+				<td></td>
+				<td>4</td>
+				<td>9776</td>
+				<td>626</td>
+				<td></td>
+				<td>126</td>
+				<td></td>
+				<td></td>
+				<td>97</td>
+				<td>6165</td>
+				<td>6626</td>
+			</tr>
+			<tr class="row-three">
+				<td>834</td>
+				<td></td>
+				<td></td>
+				<td>729111</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td>89</td>
+				<td>6589</td>
+				<td>6547</td>
+				<td>3465</td>
+				<td></td>
+				<td></td>
+				<td>867</td>
+				<td>2347</td>
+				<td>5762</td>
+			</tr>
+			<tr class="row-four">
+				<td>4768</td>
+				<td></td>
+				<td></td>
+				<td>8967284</td>
+				<td></td>
+				<td></td>
+				<td>7</td>
+				<td>9798</td>
+				<td>8969</td>
+				<td>476</td>
+				<td>8476</td>
+				<td></td>
+				<td></td>
+				<td>9479</td>
+				<td>982</td>
+				<td>8969</td>
+			</tr>
+			<tr class="row-five">
+				<td>685</td>
+				<td></td>
+				<td></td>
+				<td>3478</td>
+				<td></td>
+				<td></td>
+				<td>8</td>
+				<td>864</td>
+				<td>7146</td>
+				<td>347</td>
+				<td>429</td>
+				<td></td>
+				<td></td>
+				<td>840</td>
+				<td></td>
+				<td>897</td>
+			</tr>
+			<tr class="row-six">
+				<td>757</td>
+				<td></td>
+				<td></td>
+				<td>898990</td>
+				<td></td>
+				<td></td>
+				<td>1</td>
+				<td>200</td>
+				<td>285</td>
+				<td>923</td>
+				<td>387</td>
+				<td></td>
+				<td></td>
+				<td>238</td>
+				<td>578</td>
+				<td>5875</td>
+			</tr>
+			<tr class="row-seven">
+				<td>484</td>
+				<td></td>
+				<td></td>
+				<td>947589</td>
+				<td></td>
+				<td></td>
+				<td>6</td>
+				<td>569</td>
+				<td>68</td>
+				<td>678</td>
+				<td>56</td>
+				<td></td>
+				<td></td>
+				<td>584</td>
+				<td>678</td>
+				<td>476</td>
+			</tr>
+			<tr class="row-eight">
+				<td>8206</td>
+				<td></td>
+				<td></td>
+				<td>4735</td>
+				<td></td>
+				<td></td>
+				<td>5</td>
+				<td>201</td>
+				<td>647</td>
+				<td>1863</td>
+				<td>933</td>
+				<td></td>
+				<td></td>
+				<td>254</td>
+				<td>7913</td>
+				<td>4798</td>
+			</tr>
+			<tr class="row-nine">
+				<td>3472</td>
+				<td></td>
+				<td></td>
+				<td>1138</td>
+				<td></td>
+				<td></td>
+				<td>3</td>
+				<td>447</td>
+				<td></td>
+				<td>407</td>
+				<td>676</td>
+				<td></td>
+				<td></td>
+				<td>99</td>
+				<td>7103</td>
+				<td>3819</td>
+			</tr>
+			<tr class="row-ten">
+				<td>1701</td>
+				<td></td>
+				<td></td>
+				<td>33397</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td>834</td>
+				<td>753</td>
+				<td>204</td>
+				<td>59</td>
+				<td></td>
+				<td></td>
+				<td>806</td>
+				<td>2042</td>
+				<td>5047</td>
+			</tr>
+		</tbody>
+	</table>
 `;
 
 // Button functions
-document.querySelectorAll('.gray').forEach((b) => {b.onclick = beep1});
+document.querySelectorAll('.button-container > .gray').forEach((b) => {b.onclick = beep1});
+document.querySelectorAll('#button-panel-1, #label').forEach((b) => {b.onclick = beep2});
 document.querySelectorAll('.orange').forEach((b) => {b.onclick = beep3});
 
 const beepOne = new Audio('https://noeltucker.github.io/federation-database/audio/beep-1.mp3');
@@ -246,39 +245,29 @@ const beepTwo = new Audio('https://noeltucker.github.io/federation-database/audi
 const beepThree = new Audio('https://noeltucker.github.io/federation-database/audio/beep-3.mp3');
 
 function beep1(){
-  beepOne.play();
+	beepOne.play();
 }
 
 function beep2(){
-  beepTwo.play();
+	beepTwo.play();
 }
 
 function beep3(){
-  beepThree.play();
+	beepThree.play();
 }
-
-document.querySelector('#button-nine').onclick = function(){
-  beep2();
-  setTimeout(() => {window.location.reload()}, 1000);
-};
-
-document.querySelector('#button-ten').onclick = function(){
-  beep2();
-  setTimeout(() => {window.open(location, '_self').close()}, 1000);
-};
 
 // Heading IDs
 function addHeadingIds(){
-  const headings = document.querySelectorAll('h2, h3, h4, h5, h6');
-  headings.forEach(heading => {
-    let headingId = heading.innerText;
-    headingId = headingId.toLowerCase();
-    headingId = headingId.normalize('NFD').replace(/\p{Diacritic}/gu, '');
-    headingId = headingId.replace(/[\s\u2013\u2014]/g, '-');
-    headingId = headingId.replace(/([a-zA-Z])\./g, '$1');
-    headingId = headingId.replace(/['":;,]/g, '');
-    heading.id = headingId;
-  });
+	const headings = document.querySelectorAll('h2, h3, h4, h5, h6');
+	headings.forEach(heading => {
+		let headingId = heading.innerText;
+		headingId = headingId.toLowerCase();
+		headingId = headingId.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+		headingId = headingId.replace(/[\s\u2013\u2014]/g, '-');
+		headingId = headingId.replace(/([a-zA-Z])\./g, '$1');
+		headingId = headingId.replace(/['":;,]/g, '');
+		heading.id = headingId;
+	});
 }
 
 addHeadingIds();
