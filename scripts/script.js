eb1fc2d8119b5c1340a9c7f3026641276842f96d
Noel Tucker <noelstucker@gmail.com>
2026-04-05 13:59:21 -0400

    Fix

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ document.querySelector('#panel-3').outerHTML = `
 			<div id="button-6" class="button orange">06-906308</div>
 		</div>
 	</div>
-	<div id="panel-3"><!--
+	<article id="panel-3" class="panel"><!--
 		<div id="container-2" class="button-container">
 			<div id="button-7" class="button gray">2561-395</div>
 			<div id="button-8" class="button gray">3405-465</div>
@@ -42,7 +42,7 @@ document.querySelector('#panel-3').outerHTML = `
 			<p>The Federation Database is not and does not claim to be endorsed by, sponsored by, or affiliated with Paramount Skydance, the Star Trek franchise, or any affiliated entity.</p>
 			<p>All Star Trek trademarks and copyrights on the Federation Database are owned by Paramount Skydance or an affiliated entity and are used in a way believed to qualify as fair use under United States copyright law.</p>
 		</footer>-->
-	</div>
+	</article>
 	<div id="button-panel-1" class="button gray">LCARS 42028</div>
 	<div id="button-panel-2" class="button orange">6539-456</div>
 	<table id="cascade">
