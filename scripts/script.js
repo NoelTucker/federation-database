e321dba23887c5561b2d6efd4847ddb978754a4f
Noel Tucker <noelstucker@gmail.com>
2026-04-05 21:29:49 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,10 @@ const title = document.head.appendChild(document.createElement('title'));
 title.innerHTML = document.querySelector('h1').innerHTML === 'Home' ? 'Federation Database' : `${document.querySelector('h1').innerHTML} | Federation Database`;
 
 // Design
-document.querySelector('#panel-3').outerHTML = `
+document.querySelector('#page-content').outerHTML = `
 	<div id="left-container">
 		<div id="panel-1" class="panel"></div>
-		<div id="panel-2" class="panel">
-			<div id="container-1" class="button-container">
-				<div id="button-1" class="button orange">01-558695</div>
-				<div id="button-2" class="button orange">02-771423</div>
-				<div id="button-3" class="button orange">03-109549</div>
-				<div id="button-4" class="button gray">04-896790</div>
-				<div id="button-5" class="button gray">05-947165</div>
-				<div id="button-6" class="button orange">06-906308</div>
-			</div>
-		</div>
-		<div id="button-panel-1" class="button gray">LCARS 42028</div>
-		<div id="button-panel-2" class="button orange">6539-456</div>
+		<div id="control-button-1" class="button gray">LCARS 42028</div>
 		<table id="cascade">
 			<tbody>
 				<tr class="row-one">
@@ -202,40 +191,55 @@ document.querySelector('#panel-3').outerHTML = `
 				</tr>
 			</tbody>
 		</table>
+		<div id="panel-2" class="panel">
+			<div id="button-container-1" class="button-container">
+				<div id="button-1" class="button orange">01-558695</div>
+				<div id="button-2" class="button orange">02-771423</div>
+				<div id="button-3" class="button orange">03-109549</div>
+				<div id="button-4" class="button gray">04-896790</div>
+				<div id="button-5" class="button gray">05-947165</div>
+				<div id="button-6" class="button orange">06-906308</div>
+			</div>
+		</div>
+		<div id="control-button-2" class="button orange">6539-456</div>
 	</div>
-	<article id="panel-3" class="panel">
-		<a id="label" href="https://noeltucker.github.io/federation-database">Federation Database</a>
-		<div id="container-2" class="button-container">
-			<div id="button-7" class="button gray">2561-395</div>
-			<div id="button-8" class="button gray">3405-465</div>
-			<div id="button-9" class="button gray">01-767386</div>
-			<div id="button-10" class="button gray">02-406950</div>
-			<div id="button-11" class="button gray">03-883192</div>
-			<div id="button-12" class="button gray">04-547632</div>
-			<div id="button-13" class="button orange">05-521358</div>
-			<div id="button-14" class="button orange">06-684421</div>
+	<div id="right-container">
+		<div id="panel-3" class="panel">
+			<a id="label" href="https://noeltucker.github.io/federation-database">Federation Database</a>
+			<div id="button-container-2" class="button-container">
+				<div id="button-7" class="button gray">2561-395</div>
+				<div id="button-8" class="button gray">3405-465</div>
+				<div id="button-9" class="button gray">01-767386</div>
+				<div id="button-10" class="button gray">02-406950</div>
+				<div id="button-11" class="button gray">03-883192</div>
+				<div id="button-12" class="button gray">04-547632</div>
+				<div id="button-13" class="button orange">05-521358</div>
+				<div id="button-14" class="button orange">06-684421</div>
+			</div>
+			<div id="button-container-3" class="button-container">
+				<div id="button-15" class="button gray">01-906788</div>
+				<div id="button-16" class="button gray">02-786195</div>
+				<div id="button-17" class="button gray">03-855803</div>
+				<div id="button-18" class="button gray">04-261697</div>
+			</div>
 		</div>
-		<div id="container-3" class="button-container">
-			<div id="button-15" class="button gray">01-906788</div>
-			<div id="button-16" class="button gray">02-786195</div>
-			<div id="button-17" class="button gray">03-855803</div>
-			<div id="button-18" class="button gray">04-261697</div>
-		</div>
-		${document.querySelector('#panel-3').innerHTML}
-		<footer id="footer">
-			<p>Text &copy; 2024&ndash;2025 Noel Tucker</p>
-			<p>All quotations on the Federation Database are used in a way believed to quality as fair use under United States copyright law. All other text is owned by Noel Tucker and is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0">Creative Commons Attribution-ShareAlike 4.0 International license</a>.</p>
-			<p>The design of the Federation Database is inspired by the LCARS design style created by Michael Okuda. The colors are from LCARS designs by Andrew Jarvis. All original aspects are owned by Noel Tucker and are not available under any license. All unoriginal aspects are owned by <a href="https://www.paramount.com">Paramount Skydance</a> and are used in a way believed to qualify as fair use under United States copyright law.</p>
-			<p>License and/or copyright information is listed individually for all images, video, and audio on the Federation Database.</p>
-			<p>The Federation Database is not and does not claim to be endorsed by, sponsored by, or affiliated with Paramount Skydance, the Star Trek franchise, or any affiliated entity.</p>
-			<p>All Star Trek trademarks and copyrights on the Federation Database are owned by Paramount Skydance or an affiliated entity and are used in a way believed to qualify as fair use under United States copyright law.</p>
-		</footer>
-	</article>
+		<article id="page-content">
+			${document.querySelector('#right-container').innerHTML}
+			<footer id="footer">
+				<p>Text &copy; 2024&ndash;2025 Noel Tucker</p>
+				<p>All quotations on the Federation Database are used in a way believed to quality as fair use under United States copyright law. All other text is owned by Noel Tucker and is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0">Creative Commons Attribution-ShareAlike 4.0 International license</a>.</p>
+				<p>The design of the Federation Database is inspired by the LCARS design style created by Michael Okuda. The colors are from LCARS designs by Andrew Jarvis. All original aspects are owned by Noel Tucker and are not available under any license. All unoriginal aspects are owned by <a href="https://www.paramount.com">Paramount Skydance</a> and are used in a way believed to qualify as fair use under United States copyright law.</p>
+				<p>License and/or copyright information is listed individually for all images, video, and audio on the Federation Database.</p>
+				<p>The Federation Database is not and does not claim to be endorsed by, sponsored by, or affiliated with Paramount Skydance, the Star Trek franchise, or any affiliated entity.</p>
+				<p>All Star Trek trademarks and copyrights on the Federation Database are owned by Paramount Skydance or an affiliated entity and are used in a way believed to qualify as fair use under United States copyright law.</p>
+			</footer>
+		</article>
+	</div>
 `;
 
 // Button functions
 document.querySelectorAll('.button-container > .gray').forEach((b) => {b.onclick = beep1});
-document.querySelectorAll('#button-panel-1, #label').forEach((b) => {b.onclick = beep2});
+document.querySelectorAll('#control-button-1, #label').forEach((b) => {b.onclick = beep2});
 document.querySelectorAll('.orange').forEach((b) => {b.onclick = beep3});
 
 const beepOne = new Audio('https://noeltucker.github.io/federation-database/audio/beep-1.mp3');
